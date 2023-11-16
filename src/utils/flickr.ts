45ae95f9b87773eb1da78954e65ddd76b4214d2b
Tobin Brown <tobinbrown@appliedsystems.com>
2023-11-16 00:35:04 +0000

    flickr v7

--- src/utils/flickr.ts
+++ src/utils/flickr.ts
@@ -1,19 +1,18 @@
 import { Photo, PhotoExifData } from "@brobin/types/flickr";
-import Flickr from "flickr-sdk";
+import { createFlickr } from "flickr-sdk";
 
 const apiKey = process.env.FLICKR_API_KEY as string;
 const userId = process.env.FLICKR_USER_ID as string;
 
-const flickr = new Flickr(apiKey);
+const { flickr } = createFlickr(apiKey);
 
 export async function getPhotos(): Promise<Photo[]> {
-  return await flickr.people
-    .getPhotos({
-      user_id: userId,
-      extras: "description,date_taken,url_o,url_l,tags",
-      per_page: "50",
-    })
-    .then((res: { text: string }) => JSON.parse(res.text)["photos"]["photo"])
+  return flickr("flickr.people.getPhotos", {
+    user_id: userId,
+    extras: "description,date_taken,url_o,url_l,tags",
+    per_page: "50",
+  })
+    .then((data) => data.photos.photo)
     .catch(() => []);
 }
 
@@ -24,20 +23,17 @@ export async function getPhoto(photo_id: string) {
 export async function getPhotoExifData(
   photo_id: string
 ): Promise<PhotoExifData> {
-  return flickr.photos
-    .getExif({ photo_id })
-    .then((res) => JSON.parse(res.text))
-    .then((data) => {
-      const tag = (tag: string) =>
-        data.photo.exif.find((e: any) => e.tag === tag)?.raw._content || null;
+  return flickr("flickr.photos.getExif", { photo_id }).then((data) => {
+    const tag = (tag: string) =>
+      data.photo.exif.find((e: any) => e.tag === tag)?.raw._content || null;
 
-      return {
-        camera: data.photo.camera || null,
-        lens: tag("LensModel") || tag("Lens"),
-        exposure: tag("ExposureTime"),
-        iso: tag("ISO"),
-        focalLength: tag("FocalLength"),
-        aperture: tag("FNumber"),
-      };
-    });
+    return {
+      camera: data.photo.camera || null,
+      lens: tag("LensModel") || tag("Lens"),
+      exposure: tag("ExposureTime"),
+      iso: tag("ISO"),
+      focalLength: tag("FocalLength"),
+      aperture: tag("FNumber"),
+    };
+  });
 }
