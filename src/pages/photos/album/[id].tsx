879d5fa1eb9abd5cad5539b9e7ba67bd3e7b87b5
Tobin Brown <tobinbrown@appliedsystems.com>
2023-11-17 04:33:35 +0000

    image list columns breakpoints

--- src/pages/photos/album/[id].tsx
+++ src/pages/photos/album/[id].tsx
@@ -3,18 +3,28 @@ import PhotoContainer from "@brobin/components/photos/PhotoContainer";
 import { AlbumDetail } from "@brobin/types/flickr";
 import { getAlbumDetail } from "@brobin/utils/flickr";
 import { Divider, Typography } from "@mui/joy";
-import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
+import {
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 interface Props {
   album: AlbumDetail;
 }
 
 export default function Album({ album }: Props) {
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.down("md"));
+  const sm = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Page title={`${album.title} | Photos`}>
       <Typography level="h1">{album.title}</Typography>
       <Divider sx={{ marginY: 2 }} />
-      <ImageList gap={10} cols={3} variant="masonry">
+      <ImageList gap={10} cols={sm ? 1 : md ? 2 : 3} variant="masonry">
         {album.photos.map((photo) => (
           <ImageListItem
             key={photo.id}
@@ -22,10 +32,7 @@ export default function Album({ album }: Props) {
             href={`/photos/photo/${photo.id}`}
           >
             <PhotoContainer title={photo.title} size={photo.medium} />
-            <ImageListItemBar
-              title={photo.title}
-              sx={{ display: { xs: "none", md: "block" } }}
-            />
+            <ImageListItemBar title={photo.title} />
           </ImageListItem>
         ))}
       </ImageList>
--- src/pages/photos/index.tsx
+++ src/pages/photos/index.tsx
@@ -3,18 +3,28 @@ import PhotoContainer from "@brobin/components/photos/PhotoContainer";
 import { Album } from "@brobin/types/flickr";
 import { getAlbums } from "@brobin/utils/flickr";
 import { Divider, Typography } from "@mui/joy";
-import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
+import {
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 interface Props {
   albums: Album[];
 }
 
 export default function Albums({ albums }: Props) {
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.down("md"));
+  const sm = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Page title="Photos">
       <Typography level="h1">Photos</Typography>
       <Divider sx={{ marginY: 2 }} />
-      <ImageList gap={10} cols={3} variant="masonry">
+      <ImageList gap={10} cols={sm ? 1 : md ? 2 : 3} variant="masonry">
         {albums.map((album) => (
           <ImageListItem
             key={album.id}
@@ -22,10 +32,7 @@ export default function Albums({ albums }: Props) {
             href={`/photos/album/${album.id}`}
           >
             <PhotoContainer title={album.title} size={album.primary} />
-            <ImageListItemBar
-              title={`${album.title} (${album.total})`}
-              sx={{ display: { xs: "none", md: "block" } }}
-            />
+            <ImageListItemBar title={`${album.title} (${album.total})`} />
           </ImageListItem>
         ))}
       </ImageList>
