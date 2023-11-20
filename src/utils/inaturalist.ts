b6c5447d62cab6217a5d8dc96a00d119dc64de4a
Tobin Brown <tobinbrown@appliedsystems.com>
2023-11-20 19:13:48 +0000

    include subspecies

--- src/utils/inaturalist.ts
+++ src/utils/inaturalist.ts
@@ -13,7 +13,7 @@ function get<T>(url: string): Promise<T | null> {
 
 export async function searchTaxonomy(search: string): Promise<Taxon[]> {
   return get<INaturalistTaxon>(
-    `https://api.inaturalist.org/v1/taxa?q=${search}&rank=${Rank.Species}`
+    `https://api.inaturalist.org/v1/taxa?q=${search}&rank=${Rank.Species}&rank=${Rank.SubSpecies}`
   ).then(async (taxon) => {
     if (taxon) {
       const detail = await get<INaturalistTaxonDetail>(
@@ -22,7 +22,7 @@ export async function searchTaxonomy(search: string): Promise<Taxon[]> {
       const taxonomy = detail?.ancestors || [];
       return taxonomy
         .sort((a, b) => b.rank_level - a.rank_level)
-        .filter((t) => t.rank_level % 10 === 0)
+        .filter((t) => t.rank_level % 10 === 0 || t.rank_level < 10)
         .concat([taxon])
         .map(({ id, rank, name, wikipedia_url, preferred_common_name }) => ({
           id,
