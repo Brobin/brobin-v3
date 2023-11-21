3a8d2553f3221704efd93cce47436df51dcd22b9
Tobin Brown <tobinbrown@appliedsystems.com>
2023-11-21 18:35:05 +0000

    cache pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@mui/x-charts"],
+  async headers() {
+    return [
+      {
+        source: "/:photos*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=86400, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
