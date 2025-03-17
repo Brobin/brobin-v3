ee9db652999924d95b1c1aa805ec0cd1cc1bbf65
Tobin Brown <tobinbrown@appliedsystems.com>
2025-03-17 18:44:07 +0000

    fix: aspostrophes

--- src/pages/big-year.tsx
+++ src/pages/big-year.tsx
@@ -27,11 +27,11 @@ export default function BigYear({ birds, series }: Props) {
     <Page title="Nebraska Big Year 2025">
       <Typography level="h1">Nebraska Big Year 2025</Typography>
       <p>
-        I'm spending 2025 finding as many birds as I can in the state of
+        I&apos;m spending 2025 finding as many birds as I can in the state of
         Nebraska! The current record is 347, and my goal is 340. If things go
-        well, 350 will be my stretch goal, but I'll be happy with any result,
-        knowing that I'm doing my best to find all the birds that pass through
-        or breed in the state.
+        well, 350 will be my stretch goal, but I&apos;ll be happy with any
+        result, knowing that I&apos;m doing my best to find all the birds that
+        pass through or breed in the state.
       </p>
       <ButtonGroup variant="outlined">
         <Button href="https://ebird.org/tripreport/325880" component="a">
