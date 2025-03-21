0ca0eee018c968550b487f29ab01f3d933cad84e
Tobin Brown <tobinbrown@appliedsystems.com>
2025-03-21 19:03:35 +0000

    feat: add discord bot endpoint for rare birds

---
+++ src/pages/api/rare-birds.ts
@@ -0,0 +1,54 @@
+import { getRareBirds } from "@brobin/utils/ebird";
+import dayjs from "dayjs";
+import { Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
+import { NextApiRequest, NextApiResponse } from "next";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { neon } from "@neondatabase/serverless";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("America/Chicago");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  await client.login(process.env.DISCORD_TOKEN);
+  const channel = await client.channels.fetch(
+    process.env.DISCORD_CHANNEL_ID as string
+  );
+
+  const observations = await getRareBirds();
+  const sql = neon(`${process.env.DATABASE_URL}`);
+
+  if (channel && channel.isSendable()) {
+    const result = await sql("SELECT obsId from rare_birds");
+
+    const alreadyInserted = result.map((result) => result["obsId"]);
+
+    observations
+      .filter((obs) => !alreadyInserted.includes(obs.obsId))
+      .forEach(async (obs) => {
+        const location = obs.locationPrivate
+          ? `${obs.locName} (Personal Location)`
+          : obs.locName;
+        const message = new EmbedBuilder()
+          .setColor(0x0099ff)
+          .setTitle(obs.comName)
+          .setURL(`https://ebird.org/checklist/${obs.subId}`)
+          .setTimestamp(dayjs(obs.obsDt).tz("America/Chicago", true).toDate())
+          .addFields({
+            value: location,
+            name: `${obs.subnational2Name} County`,
+          })
+          .setFooter({ text: obs.userDisplayName });
+
+        channel.send({ embeds: [message] });
+        await sql("INSERT INTO rare_birds (obsId) VALUES ($1)", [obs.obsId]);
+      });
+  }
+
+  res.status(200).json(observations);
+}
--- src/utils/ebird.ts
+++ src/utils/ebird.ts
@@ -4,15 +4,14 @@ const apiToken = process.env.EBIRD_API_TOKEN as string;
 
 export async function getRareBirds(): Promise<Observation[]> {
   const observations: Observation[] = await fetch(
-    "https://api.ebird.org/v2/data/obs/US-NE/recent/notable?detail=full&back=1",
+    "https://api.ebird.org/v2/data/obs/US-NE/recent/notable?detail=full&back=2",
     { headers: { "X-eBirdApiToken": apiToken } }
   )
     .then((response) => response.json())
     .then((observations) =>
-      // Get the first obs for each
       observations.sort((a: Observation, b: Observation) => {
         if (a.subId > b.subId) return 1;
-        if (a.subId < b.subId) return -1;
+        if (a.subId < b.subId) return -1; // Get the first obs for each
         return 0;
       })
     )
@@ -22,9 +21,9 @@ export async function getRareBirds(): Promise<Observation[]> {
 
   // Remove duplicates from shared checklists
   observations.forEach((obs) => {
-    let exists = uniqueObservations.filter(
+    let exists = uniqueObservations.some(
       (u) => u.obsDt === obs.obsDt && u.comName === obs.comName
-    ).length;
+    );
     if (!exists) {
       uniqueObservations.push(obs);
     }
@@ -32,8 +31,8 @@ export async function getRareBirds(): Promise<Observation[]> {
 
   // Order by date desc
   return uniqueObservations.sort((a, b) => {
-    if (a.obsDt > b.obsDt) return -1;
-    if (a.obsDt < b.obsDt) return 1;
+    if (a.obsDt > b.obsDt) return 1;
+    if (a.obsDt < b.obsDt) return -1;
     return 0;
   });
 }
