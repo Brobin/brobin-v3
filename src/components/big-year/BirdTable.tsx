0a03c6778a93c27a8d042c58c0165aa0b11cfa45
Tobin Brown <tobinbrown@appliedsystems.com>
2025-11-03 16:44:45 +0000

    feat: add search and filter to bird table

--- src/components/big-year/BirdTable.tsx
+++ src/components/big-year/BirdTable.tsx
@@ -2,8 +2,22 @@ import { Bird } from "@brobin/types/big-year";
 import { CameraAlt, Check, VolumeUp } from "@mui/icons-material";
 import { Link } from "@mui/joy";
 import { Chip } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarFilterButton,
+  GridToolbarQuickFilter,
+} from "@mui/x-data-grid";
 import dayjs from "dayjs";
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarQuickFilter />
+      <GridToolbarFilterButton sx={{ float: "right", marginLeft: "auto" }} />
+    </GridToolbarContainer>
+  );
+}
 
 export default function BirdTable({ birds }: { birds: Bird[] }) {
   return (
@@ -18,28 +32,33 @@ export default function BirdTable({ birds }: { birds: Bird[] }) {
       }}
       pageSizeOptions={[]}
       columns={[
-        { field: "id", headerName: "#", width: 40 },
+        { field: "id", headerName: "#", width: 40, disableColumnMenu: true },
         {
           field: "name",
           headerName: "Species",
           width: 240,
+          disableColumnMenu: true,
         },
 
         {
           field: "location",
           headerName: "Location",
           sortable: false,
+          disableColumnMenu: true,
         },
         {
           field: "date",
           headerName: "Date",
           width: 70,
+          disableColumnMenu: true,
           valueFormatter: ({ value }) => dayjs(value).format("MMM DD"),
         },
         {
           field: "photoId",
           headerName: "",
           width: 10,
+          sortable: false,
+          disableColumnMenu: true,
           renderCell({ value }) {
             if (value) {
               return (
@@ -52,12 +71,15 @@ export default function BirdTable({ birds }: { birds: Bird[] }) {
                 </Link>
               );
             }
+            return undefined;
           },
         },
         {
           field: "audioId",
           headerName: "",
           width: 10,
+          sortable: false,
+          disableColumnMenu: true,
           renderCell({ value }) {
             if (value) {
               return (
@@ -70,12 +92,16 @@ export default function BirdTable({ birds }: { birds: Bird[] }) {
                 </Link>
               );
             }
+            return undefined;
           },
         },
         {
           field: "lifeBird",
+          type: "boolean",
           headerName: "",
           width: 76,
+          sortable: false,
+          disableColumnMenu: true,
           renderCell({ value }) {
             if (value) {
               return (
@@ -95,6 +121,8 @@ export default function BirdTable({ birds }: { birds: Bird[] }) {
           field: "stateBird",
           headerName: "",
           width: 85,
+          sortable: false,
+          disableColumnMenu: true,
           renderCell({ value }) {
             if (value) {
               return (
@@ -112,6 +140,7 @@ export default function BirdTable({ birds }: { birds: Bird[] }) {
         },
       ]}
       rowHeight={38}
+      slots={{ toolbar: CustomToolbar }}
     />
   );
 }
